98b4ae073959e3a6c714e581f2429bceedd81ddd
Ramy Medhat <ramymedhat97@gmail.com>
2024-03-18 18:25:41 +0200

    product_view

--- products/views.py
+++ products/views.py
@@ -1,15 +1,11 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView ,ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
 from .models import Product
 from .serializers import ProductSerializer
 
-class ProductList(ListAPIView):
+class ProductList(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 class ProductDetail(RetrieveAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
-class ProductList(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
